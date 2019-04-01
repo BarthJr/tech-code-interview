f8cd78274a4a1e1d10c7c2d4cf181a4a465297cc
BarthJr <juniior.barth@gmail.com>
2019-04-01 20:57:50 -0300

    improvement in solution

--- 4sum_ii.py
+++ 4sum_ii.py
@@ -14,17 +14,10 @@
 # The two tuples are:
 # 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 # 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
-
+from collections import Counter
 from typing import List
 
 
 def four_sum_count(A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
-    hash_table, count = {}, 0
-    for i in A:
-        for j in B:
-            hash_table[i + j] = hash_table.get(i + j, 0) + 1
-
-    for i in C:
-        for j in D:
-            count += hash_table.get(-i - j, 0)
-    return count
+    hash_table = Counter(i + j for i in A for j in B)
+    return sum(hash_table[-i - j] for i in C for j in D)
