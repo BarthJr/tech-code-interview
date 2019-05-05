01798bd0cbc732be53d56ea595cc8c1338c677b3
Barth <juniior.barth@gmail.com>
2019-05-05 11:03:56 -0300

    Solution for find number of islands using DFS

--- number_of_islands.py
+++ number_of_islands.py
@@ -10,6 +10,7 @@
 >>> grid = [['1', '1', '0', '0', '0'], ['1', '1', '0', '0', '0'], ['0', '0', '1', '0', '0'], ['0', '0', '0', '1', '1']]
 >>> num_islands(grid)
 3
+
 >>> grid = [['0', '0', '0', '0', '0']]
 >>> num_islands(grid)
 0
@@ -21,62 +22,23 @@
 from typing import List
 
 
-def find_lands(grid, visited):
-    lands = set()
-    lin_len = len(grid)
-    col_len = len(grid[0])
-    for i in range(lin_len):
-        for j in range(col_len):
-            if grid[i][j] == '1' and (i, j) not in visited:
-                lands.add((i, j))
-    return lands
-
-
-def adjacents(grid, position):
-    i, j = position
-
-    line_length = len(grid)
-    col_length = len(grid[0])
-    res = [
-        (max(i - 1, 0), j),
-        (min(i + 1, line_length - 1), j),
-        (i, max(j - 1, 0)),
-        (i, min(j + 1, col_length - 1))]
-    return res
-
-
 def num_islands(grid: List[List[str]]) -> int:
     num_islands = 0
+
     if not grid:
         return num_islands
-    visited = set()
-    lands = find_lands(grid, visited)
-    if not lands:
-        return num_islands
-    to_be_visited = set([lands.pop()])
-    if None in to_be_visited:
-        return num_islands
-    while to_be_visited:
-        has_parent_land = False
-        while to_be_visited:
-            current_position = to_be_visited.pop()
-            visited.add(current_position)
-            has_parent_land = True
 
-            for p in adjacents(grid, current_position):
-                lin, col = p
-                if (grid[lin][col] == '1') and (p not in visited):
-                    to_be_visited.add(p)
-                    if p in lands:
-                        lands.remove(p)
+    lin_len = len(grid)
+    col_len = len(grid[0])
 
-        if has_parent_land:
-            num_islands += 1
+    def dfs(i, j):
+        if 0 <= i < lin_len and 0 <= j < col_len and grid[i][j] == '1':
+            grid[i][j] = '0'
+            list(map(dfs, (i - 1, i + 1, i, i), (j, j, j - 1, j + 1)))
 
-        if not lands:
-            return num_islands
-        to_be_visited = set([lands.pop()])
-        if None in to_be_visited:
-            return num_islands
-
+    for i in range(lin_len):
+        for j in range(col_len):
+            if grid[i][j] == '1':
+                dfs(i, j)
+                num_islands += 1
     return num_islands
