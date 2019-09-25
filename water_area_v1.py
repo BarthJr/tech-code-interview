6d39671fd5df4b5da69916ce3e68533b00933513
Barth <juniior.barth@gmail.com>
2019-09-25 11:44:22 -0300

    squash! Second solution for Water Area problem

--- water_area_v1.py
+++ water_area_v1.py
@@ -1,7 +1,6 @@
 # You are given an array of integers. Each non-zero integer represents the height of a pillar of width 1.
 # Imagine water being poured over all of the pillars and return the surface area of the water trapped
 # between the pillars viewed from the front. Note that spilled water should be ignored.
-# Refer to the first minute of the video explanation below for a visual example.
 
 """
 >>> waterArea([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3])
